6b6e4f710b5e9cc61df923746e48b6fb90f0496e
Kenjilopez <kjlopezf@academia.usbbog.edu.co>
2025-09-10 10:54:40 -0500

    adjusting car.py

--- models/car.py
+++ models/car.py
@@ -2,3 +2,16 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
+
+class marca(Base):
+    __tablename__ = 'marca'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(250), nullable=False)
+    model = relationship('Model', back_populates= 'marca',cascade='all, delete-orphan')
+    
+class modelo(base):
+    __tablename__ = 'modelos'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    marca.id = Column(Integer, ForeignKey('marca.id'))
+    marca = relationship('marca', back_populates='model')
